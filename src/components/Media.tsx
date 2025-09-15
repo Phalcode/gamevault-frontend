ffc447e0d53ac56a676df2d2cf5e29ebd6da94b5
Alper Alkan <alper.alkan2000@gmail.com>
2025-09-15 22:55:04 +0200

    fix thumbs

--- src/components/Media.tsx
+++ src/components/Media.tsx
@@ -111,7 +111,7 @@ export function Media({
         <img
           src={blobUrl}
           alt={alt}
-          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+          style={{ width: "100%", height: "100%", objectFit: "contain" }}
           draggable={false}
           onClick={onClick}
         />
